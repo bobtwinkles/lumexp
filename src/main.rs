e236553c9ecd5c1365a4dd333fedfa04642bc98d
bobtwinkles <srkoser+GitHub@gmail.com>
2019-03-08 10:57:26 -0500

    Controls and better camera

--- src/main.rs
+++ src/main.rs
@@ -169,7 +169,7 @@ fn main() {
     let mut surface = GlfwSurface::new(
         WindowDim::Windowed(1280, 720),
         "Hello, world!",
-        WindowOpt::default(),
+        WindowOpt::default().hide_cursor(true),
     )
     .expect("window creation");
     let (final_composite, _) = Program::<(), (), FinalShadeInterface>::from_strings(
@@ -195,6 +195,7 @@ fn main() {
         let size = surface.size();
         compute_rectilinearize_matrix(size[1] as f32, size[0] as f32)
     };
+    let mut aspect: f32 = surface.size()[1] as f32 / surface.size()[0] as f32;
 
     let (mut geometry_buffers, vertex_count) = {
         let (geometry, indices) = gen_geometry();
@@ -242,16 +243,92 @@ fn main() {
 
     let mut colors: Vec<[f32; 4]> = (0..vertex_count).map(|_| rand_color(1.1, 1.0)).collect();
 
+    let mut look_angles = (90.0, 0.0);
+    let mut position = cgmath::Vector3::new(0.0, 0.0, 0.0);
+    // Keys: WSADQE
+    let mut key_states = [false, false, false, false, false, false];
+    let mut control_active = true;
+
     'app: loop {
         for event in surface.poll_events() {
             match event {
                 WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => {
                     break 'app;
                 }
+                WindowEvent::Key(Key::W, _, action, _) => {
+                    key_states[0] = action != Action::Release;
+                }
+                WindowEvent::Key(Key::S, _, action, _) => {
+                    key_states[1] = action != Action::Release;
+                }
+                WindowEvent::Key(Key::A, _, action, _) => {
+                    key_states[2] = action != Action::Release;
+                }
+                WindowEvent::Key(Key::D, _, action, _) => {
+                    key_states[3] = action != Action::Release;
+                }
+                WindowEvent::Key(Key::Q, _, action, _) => {
+                    key_states[4] = action != Action::Release;
+                }
+                WindowEvent::Key(Key::E, _, action, _) => {
+                    key_states[5] = action != Action::Release;
+                }
+                WindowEvent::Key(Key::Space, _, Action::Press, _) => {
+                    control_active = !control_active;
+                    if control_active {
+                        eprintln!("Controls enabled");
+                    } else {
+                        eprintln!("Controls disabled");
+                    }
+                }
+                WindowEvent::Key(Key::P, _, Action::Press, _) => {
+                    eprintln!("{:?} {:?}", position, look_angles);
+                }
+                WindowEvent::Key(Key::F1, _, Action::Press, _) => {
+                    position = cgmath::Vector3::new(4.5, -4.5, 0.55);
+                    look_angles = (94.6, -45.0);
+                }
+                WindowEvent::CursorPos(x, y) => {
+                    if control_active {
+                        look_angles.0 = 90.0 + y as f32 / 25.0;
+                        look_angles.1 = x as f32 / -25.0;
+                    }
+                }
                 WindowEvent::FramebufferSize(width, height) => {
                     resize_size = Some((width, height));
                 }
                 _ => (),
+            }
+        }
+
+        const KEY_SPEED: f32 = 0.1;
+
+        if control_active {
+            use cgmath::{Deg, Matrix3, Vector3};
+            let transform = Matrix3::from_angle_z(Deg(-look_angles.1))
+                * Matrix3::new(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, -1.0)
+                * Matrix3::from_angle_x(Deg(-look_angles.0));
+
+            let up_vector = transform * Vector3::new(0.0, KEY_SPEED, 0.0);
+            let right_vector = transform * Vector3::new(KEY_SPEED, 0.0, 0.0);
+            let look_vector = transform * Vector3::new(0.0, 0.0, KEY_SPEED);
+            if key_states[0] {
+                position += look_vector;
+            }
+            if key_states[1] {
+                position -= look_vector;
+            }
+            if key_states[2] {
+                position += right_vector;
+            }
+            if key_states[3] {
+                position -= right_vector;
+            }
+            if key_states[4] {
+                position += up_vector;
+            }
+            if key_states[5] {
+                position -= up_vector;
             }
         }
 
@@ -268,13 +345,15 @@ fn main() {
                 )
                 .expect("Blur pass resize");
             rectanglize = compute_rectilinearize_matrix(width as f32, height as f32);
+            aspect = width as f32 / height as f32;
         }
 
-        let transform = rectanglize
-            * Matrix4::from_scale(0.5)
-            * Matrix4::from_angle_x(cgmath::Deg(270.0 - 45.0))
-            * Matrix4::from_angle_z(cgmath::Deg(frame as f32))
-            ;
+        let transform = cgmath::perspective(cgmath::Deg(75.0), aspect, 0.001, 1000.0)
+            * Matrix4::from_angle_x(cgmath::Deg(look_angles.0))
+            * Matrix4::from_nonuniform_scale(1.0, 1.0, -1.0)
+            * Matrix4::from_angle_z(cgmath::Deg(look_angles.1))
+            * Matrix4::from_translation(position)
+            * Matrix4::from_angle_z(cgmath::Deg(frame as f32));
 
         let curr_geometry_buffer = frame % geometry_buffers.len();
 
@@ -287,7 +366,7 @@ fn main() {
                     interface.transform.update(transform.into());
                     render_gate.render(
                         RenderState::default().set_face_culling(FaceCulling::new(
-                            FaceCullingOrder::CW,
+                            FaceCullingOrder::CCW,
                             FaceCullingMode::Front,
                         )),
                         |tesselation_gate| {
@@ -311,9 +390,9 @@ fn main() {
 
             for i in 0..next_buffer_data.len() {
                 next_buffer_data[i].color.repr = colors[i];
-                // for j in 0..3 {
-                //     colors[i][j] = (colors[i][j] + 0.01) % 1.1;
-                // }
+                for j in 0..3 {
+                    colors[i][j] = (colors[i][j] + 0.01) % 1.1;
+                }
             }
         }
 
